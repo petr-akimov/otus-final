8a45e935783faff0cc0f5a523e1227a9783ed3af
Petr Akimov <petr.akimov@yahoo.com>
2026-04-26 21:17:06 +0300

    final

--- tmp/start-services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl delete namespace otus
-#kubectl delete namespace monitoring
-kubectl create namespace otus
-#kubectl create namespace monitoring
-kubectl apply -n otus -f k8s 
-kubectl -n otus wait --for=condition=ready pod -l app=airflow --timeout=240s 
-POD_NAME=$(kubectl -n otus get pods -l app=airflow -o name | head -1 | cut -d/ -f2)
-kubectl cp dags/drift_detection.py.dist otus/$POD_NAME:/opt/airflow/dags/drift_detection.py -c airflow 
